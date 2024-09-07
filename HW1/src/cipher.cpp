b3813f35bf6e74217bb20a6d24e43101a934597e
Ka Long Ngai <kls8162@gmail.com>
2024-09-07 12:07:10 -0400

    Updated: key_to_key_stream definition.

--- HW1/src/cipher.cpp
+++ HW1/src/cipher.cpp
@@ -8,6 +8,22 @@ VigenereCipher::VigenereCipher() {
     }
 }
 
+std::string VigenereCipher::key_to_key_stream(int length, std::string key) {
+    if (length < key.size()) {
+        key = key.substr(0, length);
+        return key;
+    }
+    int repeat_all = length / key.size();
+    std::string result;
+    while (repeat_all > 0) {
+        result += key;
+        repeat_all--;
+    }
+    int repeat_char = length % key.size();
+    for (int i = 0; i < repeat_char; i++) {
+        result += key.at(i);
+    }
+    return result;
 }
 
 void VigenereCipher::encode(std::string plain_text, std::string key) {
