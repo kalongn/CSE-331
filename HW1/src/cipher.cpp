3805fcc73c0b3975fb1fbc5d4a447af97189fd6d
Ka Long Ngai <kls8162@gmail.com>
2024-09-07 11:37:49 -0400

    Created: basic src directory.

---
+++ HW1/src/cipher.cpp
@@ -0,0 +1,17 @@
+#include "cipher.h"
+
+VigenereCipher::VigenereCipher() {
+
+}
+
+void VigenereCipher::encode(std::string plain_text, std::string key) {
+
+}
+
+void VigenereCipher::decode(std::string cipher_text, std::string key) {
+
+}
+
+void VigenereCipher::break_cipher(std::string cipher_text, int key_length) {
+
+}
