4e7efb411aa0b556b98729edaf267d65a998009f
Ka Long Ngai <kls8162@gmail.com>
2024-09-08 14:05:23 -0400

    Updated: add new functions and changed some definitions used the const & for stuff not changing / need to clone added methods for the break cipher steps.

--- HW1/include/cipher.h
+++ HW1/include/cipher.h
@@ -5,11 +5,37 @@
 #include <iostream>
 #include <string>
 #include <utility>
+#include <vector>
 #include <map>
 
 class VigenereCipher {
 private:
     const static int ALPHABET_SIZE = 26;
+
+    /**
+     * @brief Return the simplify string that only consist of alphabet characters.
+     *
+     * @param input
+     *      The input string you want to simplify.
+     * @return std::string
+     *      the simplify string
+     */
+    std::string simplify_text(const std::string &input);
+
+    /**
+     * @brief Return the a vector of splitted strings each having split_length.
+     * 
+     * @details The last one may not have the exact same size by within the range of [1, split_length].
+     * 
+     * @param input 
+     *      The input string you want to be splitted.
+     * @param split_length 
+     *      The amount of character in each split string.
+     * @return std::vector<std::string> 
+     *      The vector of string with each split string as elements.
+     */
+    std::vector<std::string> split_text(const std::string &input, int split_length);
+
 public:
 
     /**
@@ -25,11 +51,11 @@ public:
      *      The input plain text that needs to be encode.
      * @param key
      *      The key that this input plain text will encode with.
-     * 
+     *
      * @return std::string
      *      The encoded cipher text.
      */
-    std::string encode(std::string plain_text, std::string key);
+    std::string encode(const std::string &plain_text, const std::string &key);
 
     /**
      * @brief Decode a cipher text with the key given and print out the plain text.
@@ -38,11 +64,11 @@ public:
      *      The input cipher text that needs to be decode.
      * @param key
      *      The key that this input cipher text will decode with.
-     * 
+     *
      * @return std::string
      *      The decoded cipher text.
      */
-    std::string decode(std::string cipher_text, std::string key);
+    std::string decode(const std::string &cipher_text, const std::string &key);
 
     /**
      * @brief Break a cipher text into plain text doing frequency analysis and print out the plain text.
@@ -51,11 +77,11 @@ public:
      *      The input cipher text that needs to be break.
      * @param key_length
      *      The optional parameter of providing the length of the key, default to 0 means no key length provided.
-     * 
+     *
      * @return std::string
      *      The brute force decoded cipher text.
      */
-    std::string break_cipher(std::string cipher_text, int key_length = 0);
+    std::string break_cipher(const std::string &cipher_text, int key_length = 0);
 };
 
 #endif
--- HW1/src/cipher.cpp
+++ HW1/src/cipher.cpp
@@ -2,13 +2,38 @@
 
 VigenereCipher::VigenereCipher() {}
 
-std::string VigenereCipher::encode(std::string plain_text, std::string key) {
+std::string VigenereCipher::simplify_text(const std::string &input) {
+    int n = input.size();
+    std::string result;
+    for (auto i = 0; i < n; i++) {
+        char current_character = input.at(i);
+        if (isalpha(current_character)) {
+            result.push_back(tolower(current_character));
+        }
+    }
+    return result;
+}
+
+std::vector<std::string> VigenereCipher::split_text(const std::string &input, int split_length) {
+    int substrings_count = input.length() / split_length;
+    std::vector<std::string> result;
+    for (auto i = 0; i < substrings_count; i++) {
+        result.push_back(input.substr(i * split_length, split_length));
+    }
+    if (input.length() % split_length != 0) {
+        result.push_back(input.substr(split_length * substrings_count));
+    }
+    return result;
+}
+
+
+std::string VigenereCipher::encode(const std::string &plain_text, const std::string &key) {
     const int plain_text_length = plain_text.size();
     const int key_length = key.size();
 
     std::string result;
     int index_of_key = 0;
-    for (int i = 0; i < plain_text_length; i++) {
+    for (auto i = 0; i < plain_text_length; i++) {
         char current_character = plain_text.at(i);
         if (isalpha(current_character)) {
             char current_key_character = key.at(index_of_key);
@@ -25,13 +50,13 @@ std::string VigenereCipher::encode(std::string plain_text, std::string key) {
     return result;
 }
 
-std::string VigenereCipher::decode(std::string cipher_text, std::string key) {
+std::string VigenereCipher::decode(const std::string &cipher_text, const std::string &key) {
     const int cipher_text_length = cipher_text.size();
     const int key_length = key.size();
 
     std::string result;
     int index_of_key = 0;
-    for (int i = 0; i < cipher_text_length; i++) {
+    for (auto i = 0; i < cipher_text_length; i++) {
         char current_character = cipher_text.at(i);
         if (isalpha(current_character)) {
             char current_key_character = key.at(index_of_key);
@@ -48,6 +73,21 @@ std::string VigenereCipher::decode(std::string cipher_text, std::string key) {
     return result;
 }
 
-std::string VigenereCipher::break_cipher(std::string cipher_text, int key_length) {
+std::string VigenereCipher::break_cipher(const std::string &cipher_text, int key_length) {
+    std::string simple_text = simplify_text(cipher_text);
+    std::map<char, int> freq;
+    const int simple_text_length = simple_text.size();
+    for (auto i = 0; i < simple_text_length; i++) {
+        char current_character = simple_text.at(i);
+        if (isalpha(current_character)) {
+            freq[tolower(current_character)]++;
+        }
+    }
+    if (key_length == 0) {
+        // need to iterate each key length (brute force)
+        return "";
+    }
+    std::vector<std::string> splitted = split_text(simple_text, key_length);
 
+    return "";
 }
